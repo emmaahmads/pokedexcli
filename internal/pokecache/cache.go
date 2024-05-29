2aee560c9d301cefa407da7a9b20e7bc00dda54f
Emma Ahmad <emma.ahmads@gmail.com>
2024-05-29 11:14:55 +0800

    add cache as a new package

---
+++ internal/pokecache/cache.go
@@ -0,0 +1,75 @@
+package pokecache
+
+/* objectives of this package
+1. cacheEntry is a struct that contains the result of a API request with record of creation time
+   entries is the map that maps the URL string to the cache
+2. if the query was already made, result is retrived from the cache
+   otherwise make a new query and add result to the cache
+3. use mutex to protect cacheEntry map because map is not thread-safe in Go
+4. remove older entries periodically by using a time ticker
+
+byte slice
+- array of character bytes - can represent a string */
+
+import (
+	"sync"
+	"time"
+)
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
+	}
+	// once a cache is created we spawn a goroutine that uses a ticker to delete older entries
+	go c.reapLoop(interval)
+
+	return c
+}
+
+func (c *Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	// C is a channel of type Time that delivers "ticks" of "a clock" at intervals
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c *Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	// delete older cache entries
+	for k, v := range c.cache {
+		if v.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, k)
+		}
+	}
+}
+
+func (c *Cache) Add(key string, value []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       value,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	val, ok := c.cache[key]
+	return val.val, ok
+}
